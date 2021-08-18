bf97db3ac930666a563f1e176b7443791c0d6d01
Ch3kUtHaN <63643345+Ch3kUtHaN@users.noreply.github.com>
2021-08-18 12:23:12 +0530

    Update maintainance-bot.py

--- plugins/maintainance-bot.py
+++ plugins/maintainance-bot.py
@@ -7,14 +7,12 @@ from config import Config
 @Client.on_message(filters.private & filters.command(['start', 'help', 'about']))
 async def start(bot, update):
     await update.reply_text(
-        text=Config.MAINTAINANCE_TEXT,
+        text=Config.MAINTAINANCE_TEXT.format(update.from_user.first_name),
         disable_web_page_preview=True,
         reply_markup=InlineKeyboardMarkup(
-            [[
-            InlineKeyboardButton("Update Channel", url=Config.UPDATE_CHANNEL),
-            InlineKeyboardButton("Support Group", url=Config.SUPPORT_GROUP)
-            ],[
-            InlineKeyboardButton("Repo", url="https://github.com/HeimanPictures/Maintenance-Bot/")
-            ]]
+            [
+              [InlineKeyboardButton("📌 Bot Updates 📌", url=Config.UPDATE_CHANNEL)],
+              [InlineKeyboardButton("🎈 Channel 🎈", url=Config.SUPPORT_GROUP)]
+            ]
         )
     )
